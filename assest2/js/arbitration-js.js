12d3905c075f3b922b6e65c9d700052b823b6140
RESO-tech <75832054+RESO-tech@users.noreply.github.com>
2021-02-10 00:29:20 +0530

    Add files via upload

---
+++ assest2/js/arbitration-js.js
@@ -0,0 +1,11 @@
+
+		function myFunction() {
+		  document.querySelector(".arbitrator-hid").style.display = "none";
+		  document.querySelector(".arbitrator-shw").style.display = "block";
+		}
+		
+		function myFunction2() {
+		  document.querySelector(".arbitrator-shw").style.display = "none";
+		  document.querySelector(".arbitrator-hid").style.display = "block";
+		}
+		
